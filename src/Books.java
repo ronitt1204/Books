44db64719ae32747aa584d1e94918e2cabccb124
Ronitt Goswami <ronitt.goswami@MyGeorgian.ca>
2018-11-16 09:19:42 -0500

    second scommit

--- src/Books.java
+++ src/Books.java
@@ -43,5 +43,7 @@ public class Books {
             return -1;
     }
 
-  
+       public toString(){
+        System.out.println("Book Name: " + boookName + "Author: " + bookAuthor + "Price: " + price);
+       }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main
 {
     public static void main (String [] args)
     {
-        Book book1 ={" ", " ", 50};
+        TreeSet<String> treeSet = new TreeSet<>();
+        treeSet.add(new book("Harry Potter and the Philosopher's Stone", "J. K. Rowling", 50 ));
+        treeSet.add(new book("The Kite Runner", "Khaled Hosseini", 40 ));
+        treeSet.add(new book("Life of Pi", "Yann Martel", 60 ));
+        treeSet.add(new book("Pride and Prejudice", "Jane Austen", 45 ));
+
+        System.out.println("Books: "+treeSet);
     }
 }
